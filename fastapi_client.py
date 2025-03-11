0344e32b8b1e082b84910838a52f6e88f1c9b4fc
Roberto <33383872+cell32@users.noreply.github.com>
2025-03-11 18:36:27 -0500

    changed cleint script

--- fastapi_client.py
+++ fastapi_client.py
@@ -29,7 +29,7 @@ app = FastAPI()
 templates = Jinja2Templates(directory="templates")
 
 # Modbus server details
-MODBUS_SERVER_IP =  os.getenv("MODBUS_SERVER_IP", "127.0.0.1")
+MODBUS_SERVER_IP =  os.getenv("MODBUS_SERVER_IP", "modbus-sever")
 MODBUS_SERVER_PORT = int(os.getenv("MODBUS_SERVER_PORT", 5020))
 
 def is_modbus_server_running(host=MODBUS_SERVER_IP, port=MODBUS_SERVER_PORT):
